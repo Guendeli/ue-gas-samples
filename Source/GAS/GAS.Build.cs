02d53f50de6f870664abc5ae0465f48aebefbd6b
oguendeli <guendeliomar@gmail.com>
2024-03-07 14:52:59 +0400

    setting up AttributeSet

---
+++ Source/GAS/GAS.Build.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class GAS : ModuleRules
+{
+	public GAS(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks","Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		
+		PublicIncludePaths.Add("GAS/");
+	}
+}
